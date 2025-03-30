610db752fe14dbc83048d700b5fd435e40932647
rsuntk <rissu.ntk@gmail.com>
2025-03-30 13:25:17 -0400

    a04: do not include sdcardfs casefolding
    * A04 4.19.191 still have sdcardfs support.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- twrp_a04.mk
+++ twrp_a04.mk
@@ -16,9 +16,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from a04 device
 $(call inherit-product, device/samsung/a04/device.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a04/recovery/root,recovery/root)
 
 PRODUCT_DEVICE := a04
